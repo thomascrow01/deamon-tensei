93acdefd5526000a835c3ed3b78af4891a55b7da
Thomas Crow <thomascrow01@gmail.com>
2019-08-23 14:03:28 +1000

    Create introcutscene.php

---
+++ deamontensei/php/introcutscene.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+
+<head>
+	<title>Deamon Tensei</title>
+	<link rel="stylesheet" type="text/css" href="../css/main.css">
+</head>
+<body>
+	<?php
+		// start a session and include functions.php
+		session_start();
+		include 'functions.php';
+	?>
+	<video width="100%" height="100%" autoplay>
+    <source src="../introassets/intro.mp4" type="video/mp4">
+  </video>
+</body>
