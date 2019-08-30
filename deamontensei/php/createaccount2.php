84bdb5c7a6e7e1b81503ae0cc78043b213514261
Thomas Crow <thomascrow01@gmail.com>
2019-08-30 13:05:35 +1000

    Update createaccount2.php
    Unfinished code for putting the username and id into session variables

--- deamontensei/php/createaccount2.php
+++ deamontensei/php/createaccount2.php
@@ -16,16 +16,20 @@
 		$createuser_password = $_POST['password'];
 		$createuser_email = $_POST['email'];
 		$createuser_namecheck = true
+			
+		$sql_players = "SELECT * FROM deamon_tensei.players ORDER BY id";
+	
+		$result_players = $conn->query($sql_players);
 
 		// check if usernames clash
 		$checkusername = "SELECT * FROM deamon_tensei.players WHERE username";
-        $checkresult = $conn->query($checkusername);
+      		$checkresult = $conn->query($checkusername);
 
-        if($checkresult->num_rows > 0) {
-        	$createuser_namecheck = true; 
-        }else{
-        	$createuser_namecheck = false;
-    	}
+        	if($checkresult->num_rows > 0) {
+        		$createuser_namecheck = true; 
+        	}else{
+        		$createuser_namecheck = false;
+    		}
 
 
 		if($createuser_namecheck){
@@ -34,9 +38,11 @@
 		$sqlplayersadd = "INSERT INTO deamon_tensei.players (id, username, password, email, admin, datemade, lastlogin, currency1, currency2, ap1, ap2, ap3, ap4, level, exp, hp, str, mag, def, res, agi, luc, savelocation, head, body, legs, feet, hands, armr, arml, ringr, ringl, otherequip) VALUES ('$createuser_username','$createuser_password', '$createuser_email', 0, date("Y-m-d H:i:s"), date("Y-m-d H:i:s"), 0, 0, NULL, NULL, NULL, NULL, 1, 0, 10, 0, 0, 0, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)";}
 
 		if ($conn->query($sqlplayersadd)) {
-			$_SESSION['message'] = "Player has been added.";
+			$_SESSION["message"] = "Player has been added.";
+			$_SESSION["username"] = $createuser_username;
+			
 		}else{
-			$_SESSION['message'] = "An error occured.";
+			$_SESSION["message"] = "An error occured.";
 		}
 	?>
 </body>
