428465ee1ee532c7fd44a46ecfa8be018809804b
Thomas Crow <thomascrow01@gmail.com>
2019-09-20 10:18:37 +1000

    Update functions.php
    test code added for generating an image

--- deamontensei/php/functions.php
+++ deamontensei/php/functions.php
@@ -11,6 +11,8 @@
 	}
 	// Connected successfully
 
+
+	// function to retrieve string for specific language
 	function script(id, language){
 		if($language == NULL){
 			$language = "en";
@@ -20,5 +22,20 @@
 			$script_result = mysql_query("SELECT en FROM deamon_tensei.languages WHERE id = '$id'");
 		}
 		return mysql_fetch_row($script_result);
+	}
+
+	// function to retrieve image of player
+	function playerimage($_SESSION["ap1"], $_SESSION["ap2"], $_SESSION["ap3"], $_SESSION["ap4"]){
+		$img = imagecreatetruecolor(200, 200);
+		imagesavealpha($img, true);
+		$color = imagecolorallocatealpha($img, 0, 0, 0, 127);
+		imagefill($img, 0, 0, $color);
+		$text_color = imagecolorallocate($im, 0, 0, 0);
+		imagestring($im, 1, 5, 5,  $_SESSION['player_ap1'], $text_color);
+		imagestring($im, 1, 5, 10,  $_SESSION['player_ap2'], $text_color);
+		imagestring($im, 1, 5, 15,  $_SESSION['player_ap3'], $text_color);
+		imagestring($im, 1, 5, 20,  $_SESSION['player_ap4'], $text_color);
+		return imagepng($im);
+		imagedestroy($im);
 	}
 ?>
