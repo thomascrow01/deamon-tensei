84cdc665bea237a7c1f37563c450d7a92c43e5dc
Thomas Crow <thomascrow01@gmail.com>
2019-10-08 01:30:20 +1100

    Update createaccount2.php
    line 40 is not sending the query and will return as false

--- deamontensei/php/createaccount2.php
+++ deamontensei/php/createaccount2.php
@@ -15,7 +15,7 @@
 		$createuser_username = $_POST['username'];
 		$createuser_password = $_POST['password'];
 		$createuser_email = $_POST['email'];
-		$createuser_namecheck = true
+		$createuser_namecheck = true;
 			
 		$sql_players = "SELECT * FROM deamon_tensei.players ORDER BY id";
 	
@@ -35,7 +35,7 @@
 		if($createuser_namecheck){
 		// add values to the database
 
-		$sqlplayersadd = "INSERT INTO deamon_tensei.players (id, username, password, email, admin, datemade, lastlogin, currency1, currency2, ap1, ap2, ap3, ap4, level, exp, hp, str, mag, def, res, agi, luc, savelocation, head, body, legs, feet, hands, armr, arml, ringr, ringl, otherequip) VALUES ('$createuser_username','$createuser_password', '$createuser_email', 0, date("Y-m-d H:i:s"), date("Y-m-d H:i:s"), 0, 0, NULL, NULL, NULL, NULL, 1, 0, 10, 0, 0, 0, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)";}
+		$sqlplayersadd = "INSERT INTO deamon_tensei.players (id, username, password, email, admin, datemade, lastlogin, currency1, currency2, ap1, ap2, ap3, ap4, level, exp, hp, str, mag, def, res, agi, luc, savelocation, head, body, legs, feet, hands, armr, arml, ringr, ringl, otherequip) VALUES (NULL,'$createuser_username','$createuser_password', '$createuser_email', 0, date('Y-m-d H:i:s'), date('Y-m-d H:i:s'), 0, 0, NULL, NULL, NULL, NULL, 1, 0, 10, 0, 0, 0, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)";}
 
 		if ($conn->query($sqlplayersadd)) {
 			$_SESSION["message"] = "Player has been added.";
