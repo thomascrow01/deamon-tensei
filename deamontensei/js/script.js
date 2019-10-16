2cab54f9c1acb065cb8a12a64286684fa819b63b
joestaen <35913191+joestaen@users.noreply.github.com>
2019-10-16 13:27:45 +1100

    end video after playing

--- deamontensei/js/script.js
+++ deamontensei/js/script.js
@@ -1,2 +1,7 @@
 document.getElementById('video').play();
 document.getElementById('audio').play();
+document.getElementById('video').addEventListener('ended', endVideo, false);
+
+function endVideo(e) {
+    document.getElementById('video').innerHTML = "";
+}
